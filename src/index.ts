321612a4589e3689a7f299cdb41b9a4b006b030e
L.K.H.M Lakdiw <manuthlakdiw2006@gmail.com>
2025-12-22 09:38:46 +0530

    Initialize Express server with MongoDB connection and environment variable support

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import rootRouter from './routes/root';
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+
+app.use("/api/v1", rootRouter)
+
+mongoose
+    .connect(process.env.MONGO_URI as string)
+    .then(() => {
+        console.log('Connected to MongoDB')
+    })
+    .catch((error) => {
+        console.error('Error connecting to MongoDB:', error)
+    })
+
+app.listen(process.env.PORT, () => {
+    console.log("Server is running")
+})
+
