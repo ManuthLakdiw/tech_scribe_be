374d09a37c5b6bd7a1d9bb76a1bbcd7ae1b6782b
L.K.H.M Lakdiw <manuthlakdiw2006@gmail.com>
2026-01-07 20:10:06 +0530

    Update CORS configuration to include production and testing frontend URLs with specified HTTP methods

--- src/index.ts
+++ src/index.ts
@@ -9,9 +9,13 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
-app.use(
-    cors()
-)
+app.use(cors({
+    origin: [
+        "https://tech-scribe-fe.vercel.app", // ඔයාගේ Frontend URL එක (Production)
+        "http://localhost:5173"              // Localhost (Testing සඳහා)
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+}));
 
 
 app.use("/api/v1", rootRouter)
